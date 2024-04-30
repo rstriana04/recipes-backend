eb9e7d43a2d13c260f49ee636243de596c66a226
rtriana <rtriana@bancolombia.com.co>
2024-04-30 16:18:47 -0500

    First commit

---
+++ src/test/java/com/recipes/recipesbackend/RecipesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipes.recipesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
